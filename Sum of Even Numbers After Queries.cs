13c916c3d399efb24d9d735497a2bf9682cb112f
salma10 <salma10@users.noreply.github.com>
2019-10-03 23:09:57 +0600

    Create Sum of Even Numbers After Queries.cs
    We have an array A of integers, and an array queries of queries.
    For the i-th query val = queries[i][0], index = queries[i][1], we add val to A[index].  Then, the answer to the i-th query is the sum of the even values of A.
    (Here, the given index = queries[i][1] is a 0-based index, and each query permanently modifies the array A.)
    Return the answer to all queries.  Your answer array should have answer[i] as the answer to the i-th query.

---
+++ Sum of Even Numbers After Queries.cs
@@ -0,0 +1,22 @@
+ public int[] SumEvenAfterQueries(int[] A, int[][] queries) {
+        
+        int[] result = new int[queries.GetLength(0)];
+        
+        int initial = A.Where(x => x % 2 == 0).Sum();//sum_Of_Array(A);
+        
+        for(int i = 0; i < queries.GetLength(0); i++)
+        {
+            int b = queries[i][1];
+            
+            if(A[b] % 2 == 0)
+                initial = initial - A[b];    
+            
+            A[b] = A[b] + queries[i][0];
+            
+            if(A[b] % 2 == 0)
+                initial = initial + A[b];    
+            
+            result[i] = initial;
+        }
+        return result;
+    }
