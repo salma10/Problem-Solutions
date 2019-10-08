b4fec7fcd59eb03293e394f451022f5ea856619a
salma10 <salma10@users.noreply.github.com>
2019-10-08 19:52:43 +0600

    Create Design HashMap.cs
    Design a HashMap without using any built-in hash table libraries.
    To be specific, your design should include these functions:
    put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
    get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
    remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.

---
+++ Design HashMap.cs
@@ -0,0 +1,27 @@
+public class MyHashMap {
+    
+    int[] hash;
+    public MyHashMap() {
+       hash = new int[1000000];
+        for (int i = 0; i < 1000000; i++) {
+            hash[i] = -1;
+        }
+    }
+    
+    public void Put(int key, int value) {        
+             hash[key] = value;
+    }
+    
+   
+    public int Get(int key) {   
+        
+        if (key < 0 || key > 1000001)
+                return -1; 
+        
+          return hash[key];        
+    }
+    
+    public void Remove(int key) {
+        hash[key] = -1; 
+    }
+}
