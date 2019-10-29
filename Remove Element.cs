a764e3a464ca939f013fe4b38f2860d6be2c64ea
salma10 <salma10@users.noreply.github.com>
2019-10-29 10:15:30 +0600

    Create Remove Element.cs

---
+++ Remove Element.cs
@@ -0,0 +1,20 @@
+/*
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+public int RemoveElement(int[] nums, int val) {       
+        int count = 0;
+        int n = nums.Length;
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] != val)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
