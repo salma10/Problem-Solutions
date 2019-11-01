3c02f6b7f28e1e1842c0a0b82babf62fddd0c34f
salma10 <salma10@users.noreply.github.com>
2019-11-01 19:54:24 +0600

    Create Number of 1 Bits.cs

---
+++ Number of 1 Bits.cs
@@ -0,0 +1,14 @@
+/*
+Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+*/
+
+ public int HammingWeight(uint n) {      
+        var result = Convert.ToString(n, 2);       
+        var groups = result.GroupBy(i => i);   
+        foreach(var group in groups)
+        {
+            if(group.Key == '1')
+              return group.Count();
+        }
+        return 0;
+    }
