6a7716b364e9003567ae4fc26454d5eedcda74bd
salma10 <salma10@users.noreply.github.com>
2021-10-30 20:20:35 +0600

    Update Pascal's Triangle.cs

--- Pascal's Triangle.cs
+++ Pascal's Triangle.cs
@@ -36,3 +36,29 @@ public IList<IList<int>> Generate(int numRows) {
            }
         return results1;
     }
+
+//Another solution
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        List<IList<int>> result = new List<IList<int>>();
+        result.Add(new List<int>{1});
+        for(int i = 1; i < numRows; i++)
+        {
+            List<int> row = new List<int>();
+            IList<int> temp = result[i - 1];
+            for(int j = 0; j <= i; j++)
+            {
+                if(j == 0 || j == i)
+                {
+                   row.Add(1); 
+                }
+                else
+                {
+                    row.Add(temp[j] + temp[j - 1]);
+                }
+            }
+            result.Add(row);
+        }
+        return result;
+    }
+}
