b7977a247f6afbfc38423576dcb7be0ceb202576
salma10 <salma10@users.noreply.github.com>
2021-10-30 19:28:57 +0600

    Update Reshape the Matrix.cs

--- Reshape the Matrix.cs
+++ Reshape the Matrix.cs
@@ -1,5 +1,4 @@
-public int[,] MatrixReshape(int[,] nums, int r, int c) {
-        
+public int[,] MatrixReshape(int[,] nums, int r, int c) {        
         int rows = nums.GetLength(0);
         int columns = nums.GetLength(1);
         
@@ -26,5 +25,45 @@ public int[,] MatrixReshape(int[,] nums, int r, int c) {
             }               
         }
          return result;   
-        
     }
+
+//Another solution of this problem
+public class Solution {
+    public int[][] MatrixReshape(int[][] mat, int r, int c) {
+        
+        int row = mat.Length;
+        int col = mat[0].Length;
+        int[][] result = new int[r][];
+        int currRow = 0;
+        int currCol = 0;
+        int[] newRow = new int[c];
+        
+        
+        if(row * col != r * c)
+            return mat;
+        
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(currCol < c)
+                {
+                    newRow[currCol] = mat[i][j];
+                    currCol++;
+                   // Console.WriteLine(mat[i][j]);
+                }
+                else
+                {
+                    result[currRow] = newRow;
+                    newRow = new int[c];
+                    currRow++;
+                    currCol = 0;
+                    newRow[currCol] = mat[i][j];
+                    currCol++;
+                }
+            }
+        }
+        result[currRow] = newRow;
+        return result;
+    }
+}
