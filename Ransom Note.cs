ca43693e38c3b3b83673076018023656546c351f
salma10 <salma10@users.noreply.github.com>
2021-10-31 12:46:10 +0600

    Update Ransom Note.cs

--- Ransom Note.cs
+++ Ransom Note.cs
@@ -25,3 +25,26 @@ You may assume that both strings contain only lowercase letters.
             }
         }
     }
+
+//Another solution
+public bool CanConstruct(string ransomNote, string magazine) {
+        Dictionary<char,int> magazineDict = new Dictionary<char,int>();
+        
+        for(int i = 0; i < magazine.Length; i++)
+        {
+            if(magazineDict.ContainsKey(magazine[i]))
+                magazineDict[magazine[i]]++;
+            else
+               magazineDict[magazine[i]] = 1; 
+        }
+        
+        for(int i = 0; i < ransomNote.Length; i++)
+        {
+            if(magazineDict.ContainsKey(ransomNote[i]) && magazineDict[ransomNote[i]] > 0)
+                magazineDict[ransomNote[i]]--;
+            else
+               return false;
+        }
+        
+        return true;
+    }
