6852c663f8a2c6e08a94230e5ce1bf3a1e9a7227
salma10 <salma10@users.noreply.github.com>
2021-10-31 12:47:22 +0600

    Update First Unique Character in a String.cs

--- First Unique Character in a String.cs
+++ First Unique Character in a String.cs
@@ -17,3 +17,24 @@ public int FirstUniqChar(string s) {
         return -1;
         
     }
+
+//Another solution
+ public int FirstUniqChar(string s) {
+        
+        Dictionary<char,int> dict = new Dictionary<char,int>();
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(dict.ContainsKey(s[i]))
+                dict[s[i]] = -1;
+            else
+                dict[s[i]] = i;
+        }
+        
+        foreach(var item in dict)
+        {
+            if(item.Value != -1)
+                return item.Value;
+        }
+        return -1;
+    }
