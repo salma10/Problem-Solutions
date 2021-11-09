ad3f626de3e60e7af44d9927a16a6db03e4a100d
salma10 <salma10@users.noreply.github.com>
2021-11-09 10:31:04 +0600

    Update Single Number.cs

--- Single Number.cs
+++ Single Number.cs
@@ -20,3 +20,13 @@ public int SingleNumber(int[] nums) {
         }
         return result;
 }
+//Another solution
+ public int SingleNumber(int[] nums) {
+        if(nums.Length == 0)
+            return 0;
+        for(int i = 1; i < nums.Length; i++)
+        {
+            nums[0] = nums[0] ^ nums[i];
+        }
+        return nums[0];
+    }
