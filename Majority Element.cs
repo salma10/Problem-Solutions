9b9e8b66e6be986ec514d718be937296272a7618
salma10 <salma10@users.noreply.github.com>
2021-11-09 10:17:44 +0600

    Update Majority Element.cs

--- Majority Element.cs
+++ Majority Element.cs
@@ -10,3 +10,22 @@
         
         return 0;
     }
+//Another solution
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        Dictionary<int, int> count = new Dictionary<int, int>();
+        int majorityCount = nums.Length % 2 == 0 ? nums.Length / 2 : (nums.Length / 2) + 1;
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(count.ContainsKey(nums[i]))
+                count[nums[i]]++;
+            else
+                count[nums[i]] = 1;
+            if(count[nums[i]] == majorityCount)
+                return nums[i];
+        }
+        
+        return -1;
+    }
+}
