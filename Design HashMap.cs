2fce4f336317245732fa25ddc9a3e0d5a7505b37
salma10 <salma10@users.noreply.github.com>
2021-11-10 19:14:46 +0600

    Update Design HashMap.cs

--- Design HashMap.cs
+++ Design HashMap.cs
@@ -25,3 +25,25 @@ public class MyHashMap {
         hash[key] = -1; 
     }
 }
+
+//Another solution
+public class MyHashMap {
+
+    int[] hash;
+    public MyHashMap() {
+        hash = Enumerable.Repeat(-1, 1000001).ToArray();
+    }
+    
+    public void Put(int key, int value) {
+        hash[key] = value;
+    }
+    
+    public int Get(int key) {
+        return hash[key];
+    }
+    
+    public void Remove(int key) {
+        hash[key] = -1;
+    }
+}
+
