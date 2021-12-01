fffdf0ae7eeba0681eb21be766e727e77996129e
salma10 <salma10@users.noreply.github.com>
2021-12-01 19:59:47 +0600

    Update Move Zeroes.cs

--- Move Zeroes.cs
+++ Move Zeroes.cs
@@ -17,3 +17,35 @@ public void MoveZeroes(int[] nums) {
         }
     }      
     }
+//Another solution
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        
+        int pointZero = -1, pointNonZero = -1;
+        int nonZeroElement = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] == 0 && pointZero == -1)
+            {
+                pointZero = i;
+                pointNonZero = -1;
+            }
+            else if(nums[i] != 0)
+            {
+                pointNonZero = i;
+                nonZeroElement = nums[i];
+            }
+            if(pointZero != -1 && pointNonZero != -1)
+            {
+                nums[pointNonZero] = 0;
+                nums[pointZero] = nonZeroElement;
+                if(pointZero + 1 != nums.Length - 1 && nums[pointZero + 1] == 0)
+                    pointZero = pointZero + 1;
+                else
+                    pointZero = pointNonZero;
+                
+                pointNonZero = -1;
+            }
+        }
+    }
+}
