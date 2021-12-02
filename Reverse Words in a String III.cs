5888f9e037655ffd3fcf4831ec3edb1d12d74ba8
salma10 <salma10@users.noreply.github.com>
2021-12-02 11:16:05 +0600

    Update Reverse Words in a String III.cs

--- Reverse Words in a String III.cs
+++ Reverse Words in a String III.cs
@@ -1,3 +1,31 @@
 public string ReverseWords(string s) {      
         return string.Join(" ", s.Split().Select(w => new string(w.ToCharArray().Reverse().ToArray())));       
     }
+//Another solution
+public class Solution {
+    public string ReverseWords(string s) {
+        string[] splittedString = s.Split(" ");
+        s = string.Empty;
+        for(int i = 0; i < splittedString.Length; i++)
+        { 
+           s =  s + ReverseString(splittedString[i]) + " ";
+        }
+        return s.Trim();
+    }
+    
+    public string ReverseString(string input) {
+        int firstPos = 0;
+        int lastPos = input.Length - 1;
+        char[] s = input.ToCharArray();
+        while(firstPos <= lastPos)
+        {
+            char temp = s[firstPos];
+            s[firstPos] = s[lastPos];
+            s[lastPos] = temp;
+            firstPos++;
+            lastPos--;
+        }
+        
+        return new string(s);
+    }
+}
