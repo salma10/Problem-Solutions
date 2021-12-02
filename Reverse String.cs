469200bacdd68723c31042d4437c468df77f1f6a
salma10 <salma10@users.noreply.github.com>
2021-12-02 10:57:41 +0600

    Update Reverse String.cs

--- Reverse String.cs
+++ Reverse String.cs
@@ -4,3 +4,23 @@
       return new string( charArray );
 
     }
+//Another solution
+public class Solution {
+    public void ReverseString(char[] s) {
+        int i = 0, j = s.Length - 1;
+        Reverse_sub(s,i,j);
+    }   
+    public void Reverse_sub(char[] s, int i, int j) {      
+        if(i > j)
+            return;
+        else
+        {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+            Reverse_sub(s,i,j);
+        }
+    }
+}
