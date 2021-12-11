1bbf7a484a66531227e8226accbbddddd45fd260
salma10 <salma10@users.noreply.github.com>
2021-12-11 14:31:32 +0600

    Update Number of 1 Bits.cs

--- Number of 1 Bits.cs
+++ Number of 1 Bits.cs
@@ -12,3 +12,17 @@ Write a function that takes an unsigned integer and return the number of '1' bit
         }
         return 0;
     }
+//Another solution
+public class Solution {
+    public int HammingWeight(uint n) {
+        
+        int sum = 0;
+        
+        while(n != 0)
+        {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
