bfebe624564237f90ff6afdfea867b475edf141f
salma10 <salma10@users.noreply.github.com>
2022-01-24 09:37:42 +0600

    Update Detect Capital.cs

--- Detect Capital.cs
+++ Detect Capital.cs
@@ -11,3 +11,24 @@
           else
             return false;
     }
+//Another solution
+public class Solution {
+    public bool DetectCapitalUse(string word) {
+        if(word == null || word.Length == 0)
+            return true;
+        
+        if(word == word.ToUpper() || word == word.ToLower())
+                return true;
+        
+        int firstcharval = (int)word[0];
+        if(firstcharval >= 65 && firstcharval < 97)
+        {
+            if(word.Substring(1,word.Length - 1) == word.Substring(1,word.Length - 1).ToLower())
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+}
