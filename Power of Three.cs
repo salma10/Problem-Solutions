a44080c66a852c9ecfe050bbd2823a8b2ecea012
salma10 <salma10@users.noreply.github.com>
2022-03-02 13:19:55 +0600

    Update Power of Three.cs

--- Power of Three.cs
+++ Power of Three.cs
@@ -5,3 +5,7 @@ Given an integer, write a function to determine if it is a power of three.
  public bool IsPowerOfThree(int n) {
        return n>0 && 1162261467%n == 0;
     }
+//Another solution
+public bool IsPowerOfThree(int n) {
+       return (Math.Log10(n) / Math.Log10(3)) % 1 == 0;
+    }
