72f3dd3be2774872672be56ee782eb100b03a328
salma10 <salma10@users.noreply.github.com>
2022-03-02 09:53:50 +0600

    Update Is Subsequence.cs

--- Is Subsequence.cs
+++ Is Subsequence.cs
@@ -29,3 +29,26 @@ A subsequence of a string is a new string which is formed from the original stri
         return result;
         
     }
+//Another solution with improved time complexity
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        
+        if ((s == null || s.Length == 0) || (s == null && t == null))
+            return true;
+        if (t == null) 
+            return false; 
+        
+        int index = 0;
+        
+        for(int i = 0; i < t.Length; i++)
+        {
+            if(s[index] == t[i])
+                index++;
+            if(index == s.Length)
+                return true;
+        }
+        
+        return false;
+        
+    }
+}
