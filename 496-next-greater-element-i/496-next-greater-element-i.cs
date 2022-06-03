bbbfd172547f9574e71e0fef37e25ed14b0a9b88
salma10 <salma10@users.noreply.github.com>
2022-06-03 11:30:19 +0600

    Time: 192 ms (47.65%), Space: 41.4 MB (99.80%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int[] NextGreaterElement(int[] findNums, int[] nums) {
+        
+        int[] result = new int[findNums.Length];
+        
+        for(int i = 0; i < findNums.Length; i++)
+        {
+            int pos = Array.IndexOf(nums,findNums[i]);
+            result[i] = GetGreaterNo(pos,nums);
+        }
+        
+        return result;
+    }
+    
+    public static int GetGreaterNo(int pos, int[] nums)
+    {
+        if(pos >= nums.Length - 1)
+          return -1;
+          
+        int count = pos + 1;
+        
+        while(count < nums.Length)
+        {
+            if(nums[pos] < nums[count])
+              return nums[count];
+            count++;
+        }
+        
+        return -1;
+    }
+}
