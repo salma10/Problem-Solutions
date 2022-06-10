25ef3f5a4c3fdcc04446a4079047e8ec45fc03e2
salma10 <salma10@users.noreply.github.com>
2022-06-10 09:12:55 +0600

    Time: 261 ms (23.06%), Space: 55.4 MB (52.07%) - LeetHub

---
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cs
@@ -0,0 +1,29 @@
+public class NumArray {
+
+   int[] arr;
+    
+    public NumArray(int[] nums) {
+        arr = new int[nums.Count()];
+        int currSum = 0;
+        for(int i = 0; i < nums.Count(); ++i){
+            currSum += nums[i];
+            arr[i] = currSum;
+        }
+    }
+
+    public int SumRange(int i, int j) 
+    {
+        int a = arr[j];
+        int b = 0;
+        if (i != 0){
+            b = arr[i - 1];
+        }
+        return a - b;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(i,j);
+ */
