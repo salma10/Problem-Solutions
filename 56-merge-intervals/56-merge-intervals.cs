7ac8f1ab2455572d07d0c9863b5fe1b497fcf205
salma10 <salma10@users.noreply.github.com>
2022-08-07 10:59:13 +0600

    Time: 261 ms (49.59%), Space: 46.5 MB (76.90%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => { return a[0] - b[0]; });
+        List<int[]> result = new List<int[]>();
+        int count = 1;
+        for(int i = 1; i < intervals.Length; i++)
+        {
+            if(intervals[i][0] <= intervals[i - 1][1])
+            {
+                intervals[i][0] = intervals[i - 1][0];
+                intervals[i][1] = intervals[i - 1][1] > intervals[i][1] ? intervals[i - 1][1] : intervals[i][1];
+            }
+            else
+                result.Add(intervals[i - 1]);
+                
+        }
+        result.Add(intervals[intervals.Length - 1]);
+        return result.ToArray();
+    }
+}
