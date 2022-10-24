f9986808d5f44542bacf9867c6e21d31a4985c28
salma10 <salma10@users.noreply.github.com>
2022-10-24 21:17:04 +0600

    Time: 48 ms (15.38%), Space: 25.4 MB (30.77%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int FindNthDigit(int n) {
+        
+        int[] liaNum = new int[] {9, 189, 2889, 38889, 488889, 5888889, 68888889, 788888889 };
+
+        if (n < 10)
+            return n;
+
+        int i = 0;
+        for (i = 0; i < liaNum.Length; ++i) 
+        {
+            if (liaNum[i] > n) 
+                break;
+        }
+
+        n -= liaNum[i - 1];
+
+        int liStart = (int)Math.Pow(10, i) -1 + n / ( i + 1 ); 
+        int liQuot = n % (i + 1);
+
+        if (liQuot == 0) 
+            return liStart % 10;
+        else 
+            return ((liStart + 1 ) / (int)Math.Pow(10, i + 1 - liQuot)) % 10;
+    }
+}
