26e1c2620c358d0096117c75e8e43fd08fa17124
salma10 <salma10@users.noreply.github.com>
2022-11-04 21:18:21 +0600

    Update Reverse Vowels of a String.cs

--- Reverse Vowels of a String.cs
+++ Reverse Vowels of a String.cs
@@ -2,8 +2,7 @@
 Write a function that takes a string as input and reverse only the vowels of a string.
 */
 
- public string ReverseVowels(string s) {
-        
+ public string ReverseVowels(string s) {        
         char[] sArr = s.ToCharArray();
         List<char> vowel = new List<char>();
         for(int i = 0; i < s.Length; i++)
@@ -12,10 +11,8 @@ Write a function that takes a string as input and reverse only the vowels of a s
             {
                 vowel.Add(sArr[i]);
             }
-        }
-        
-        vowel.Reverse();
-        
+        }       
+        vowel.Reverse();        
         for(int i = 0; i < sArr.Length; i++)
         {
             if(sArr[i] == 'a' || sArr[i] == 'e' || sArr[i] == 'i' || sArr[i] == 'o' || sArr[i] == 'u' || sArr[i] == 'A' || sArr[i] == 'E' || sArr[i] == 'I' || sArr[i] == 'O' || sArr[i] == 'U')
